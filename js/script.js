90d5753cb44ad42e6c99b39588819c75beeb5235
Jonathan-Ultini <jonathanultini@gmail.com>
2024-08-26 15:28:50 +0200

    base migliore

--- js/script.js
+++ js/script.js
@@ -1,40 +1,26 @@
 // Selezioniamo gli elementi dal DOM
 const playButton = document.getElementById('play');
 const gridContainer = document.getElementById('grid');
-
 const difficultySelect = document.getElementById('difficulty');
 
-playButton.addEventListener('click', function () {
-  const difficulty = difficultySelect.value;
-  generateGrid(difficulty);
+playButton.addEventListener('click', () => {
+  const gridSize = parseInt(difficultySelect.value);
+  generateGrid(gridSize);
 });
 
-function generateGrid(difficulty) {
+function generateGrid(gridSize) {
   gridContainer.innerHTML = '';
 
-  let gridSize;
-  let gridClass;
+  gridContainer.style.setProperty('--num-columns', gridSize);
 
-  if (difficulty == 1) {
-    gridSize = 100;
-    gridClass = 'grid-10';
-  } else if (difficulty == 2) {
-    gridSize = 81;
-    gridClass = 'grid-9';
-  } else {
-    gridSize = 49;
-    gridClass = 'grid-7';
-  }
+  const totalCells = gridSize * gridSize;
 
-  gridContainer.className = `grid ${gridClass}`;
-
-  for (let i = 1; i <= gridSize; i++) {
+  for (let i = 1; i <= totalCells; i++) {
     const cell = document.createElement('div');
     cell.classList.add('grid-item');
-    cell.innerHTML = i;
+    cell.textContent = i;
 
-    // Aggiungiamo l'evento di click su ogni cella
-    cell.addEventListener('click', function () {
+    cell.addEventListener('click', () => {
       cell.classList.add('clicked');
       console.log(`Hai cliccato sulla cella numero: ${i}`);
     });
